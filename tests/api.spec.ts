34ad16a2d3babf6a184df09f73d13e6a6f2a4b82
JarDeVSon <jardounifanor@gmail.com>
2024-05-13 14:19:57 -0300

    refatoring some variables and scenario names

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from "@playwright/test";
 
-let tokenStorage: any;
+let authorization: any;
 let id: any;
 const API_URL = process.env.API_URL;
 
 test.describe("Suite de testes API ServRest", async () => {
-  test.beforeEach("Before Each Hook - Storage token", async ({ request }) => {
+  test.beforeEach("Before Each Hook - POST /login", async ({ request }) => {
     console.log(API_URL);
     const response = await request.post(`${API_URL}/login`, {
       headers: {
@@ -18,13 +18,13 @@ test.describe("Suite de testes API ServRest", async () => {
     });
     expect(response.status()).toBe(200);
     let responseBody = await response.json();
-    tokenStorage = responseBody.authorization;
+    authorization = responseBody.authorization;
     console.log(responseBody)
-    return tokenStorage;
+    return authorization;
 
   });
 
-  test("GET ALL API Request", async ({ request }) => {
+  test("GET /usuarios", async ({ request }) => {
     const response = await request.get(`${API_URL}/usuarios`);
 
     console.log(response)
@@ -37,10 +37,10 @@ test.describe("Suite de testes API ServRest", async () => {
  
   });
 
-  test("POST API Request", async ({ request }) => {
+  test("POST /usuarios", async ({ request }) => {
     const response = await request.post(`${API_URL}/usuarios`, {
       headers: {
-        Authorization: tokenStorage,
+        Authorization: authorization,
         "Content-Type": "application/json",
       },
       data: {
@@ -58,10 +58,10 @@ test.describe("Suite de testes API ServRest", async () => {
     console.log(responseBody)
   });
 
-  test("GET ONE SINGLE API Request", async ({ request }) => {
+  test("GET /usuarios/{_id}", async ({ request }) => {
     const response = await request.get(`${API_URL}/usuarios/${id}`, {
       headers: {
-        Authorization: tokenStorage,
+        Authorization: authorization,
         "Content-Type": "application/json",
       },
     });
@@ -72,10 +72,10 @@ test.describe("Suite de testes API ServRest", async () => {
     console.log(responseBody)
     expect(responseStatus).toBe(200);
    });
-  test("PUT API Request", async ({ request }) => {
+  test("PUT /usuarios/{_id}", async ({ request }) => {
     const response = await request.put(`${API_URL}/usuarios/${id}`, {
       headers: {
-        Authorization: tokenStorage,
+        Authorization: authorization,
         "Content-Type": "application/json",
       },
       data: {
@@ -94,10 +94,10 @@ test.describe("Suite de testes API ServRest", async () => {
     expect(responseStatus).toBe(200);
    });
 
-  test("DELETE API Request", async ({ request }) => {
+  test("DELETE /usuarios/{_id}", async ({ request }) => {
     const response = await request.delete(`${API_URL}/usuarios/${id}`, {
       headers: {
-        Authorization: tokenStorage,
+        Authorization: authorization,
         "Content-Type": "application/json",
       },
     });
